077900c6cc80f8997292558d2280ba765da129f1
Yuvraj Singh <yuvrajsteve@email.com>
2026-08-21 17:23:21 +0530

    Variables ant data tpyes

---
+++ Practice/variable.cpp
@@ -0,0 +1,45 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // int data type 
+    int number = 12;
+    cout << "Number is: " << number << " and size if int is: " << sizeof(number)  << endl;
+    cout << "=====================" << endl;
+    
+    // char data type 
+    char Alphabet = 'A';
+    cout << "Char is: " << Alphabet << " and size of char is: " << sizeof(Alphabet) << endl;
+    cout << "=====================" << endl;
+
+    // short data type 
+    short newType = 'B';
+    cout << "Short is: " << newType << " and size of short is: " << sizeof(newType) << endl;
+    cout << "=====================" << endl;
+
+    // float data type 
+    float point = 23.44;
+    cout << "Float is: " << point << " and size of float is: " << sizeof(point) << endl;
+    cout << "=====================" << endl;
+
+    // double data type 
+    double afterPoint = 23.5545;
+    cout << "Double is: " << afterPoint << " and size of: " << sizeof(afterPoint) << endl;
+    cout << "=====================" << endl;
+
+    // string data type 
+    string myName = "Yuvraj Singh";
+    cout << "String is: " << myName << " and size of string is: " << sizeof(myName) << endl;
+    cout << "=====================" << endl;
+
+    // const data type 
+    const int myNumber = 6280;
+    cout << "Const is: " << myNumber << " and size of const is: " << sizeof(myNumber) << endl;
+    cout << "=====================" << endl;
+
+
+    return 0;
+}
