2eda3135ee0b0663968c99abf69da5b75f99259a
Yuvraj Singh <yuvrajsteve@email.com>
2026-08-25 16:05:05 +0530

    the calories

--- Practice/overloading.cpp
+++ Practice/overloading.cpp
@@ -2,56 +2,31 @@
 // OPERATORS and OPERATOR OVERLOADING in C++ 
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct Vector2
-{
-    float x,y;
-
-    Vector2(float x, float y)
-        : x(x), y(y) {}
-
-    Vector2 Add(const Vector2& other) const
-    {
-        return Vector2(x + other.x, y + other.y);
-    }    
-
-    Vector2 operator+(const Vector2& other) const
-    {
-        return Add(other);
-    }
-
-     Vector2 Multiply(const Vector2& other) const
-    {
-        return Vector2(x * other.x, y * other.y);
-    } 
-
-    Vector2 operator*(const Vector2& other) const
-    {
-        return Multiply(other);
-    }
+struct Ingredient {
+    int calories;
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vector2& other)
-{
-    stream << other.x << ", " << other.y;
-    return stream;
-}
-
-int main () 
-{
-    Vector2 position(4.0f, 4.0f);
-    Vector2 speed(0.5f, 1.5f);
-    Vector2 powerup(1.1f, 1.1f);
+int operator+(Ingredient A, Ingredient B) {
+    return A.calories + B.calories;
+};
 
 
-    Vector2 result1 = position.Add(speed.Multiply(powerup));
-    Vector2 result2 = position + speed * powerup;
 
-    cout << result2 << endl;
+int main () {
 
-    cin.get();
+    Ingredient sugar;
+    sugar.calories = 100;
+
+    Ingredient butter;
+    butter.calories = 200;
+
+    int total = sugar + butter;
+
+    cout << total << " : is your total calories";
+
+
 
     return 0;
 }
