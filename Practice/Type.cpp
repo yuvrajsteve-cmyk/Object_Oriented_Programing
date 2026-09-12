d741e2251e6513c8d5139840d6fbffcb40c65f73
Yuvraj Singh <yuvrajsteve@email.com>
2026-09-12 13:19:59 +0530

    Type Convertion

---
+++ Practice/Type.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+
+   int x = 20;
+   double y = x;
+
+   cout << "Value is: " << x << endl;
+   cout << "Value is: " << fixed << y << endl;
+
+    return 0;
+}
