d68e3084c23cf4ae7afead0d08c531115f97a2e9
Yuvraj Singh <yuvrajsteve@email.com>
2026-09-14 14:39:08 +0530

    Class

---
+++ Practice/Class.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+class Employee {
+    public:
+        string Name;
+        string Company;
+        int Age;
+    
+    void introduceYourSelf() {
+        cout << "Name - " << Name << endl;
+        cout << "Company - " << Company << endl;
+        cout << "Age - " << Age << endl;
+    }
+};
+
+int main() {
+    Employee employee1;
+    employee1.Name = "Yuvraj Singh";
+    employee1.Company = "YouTube";
+    employee1.Age = 25;
+    employee1.introduceYourSelf();
+
+    cout << "==================================" << endl;
+
+    Employee employee2;
+    employee2.Name = "Satinderpal Singh";
+    employee2.Company = "Satinder Mec:";
+    employee2.Age = 24;
+    employee2.introduceYourSelf();
+    
+
+    return 0;
+}
--- Practice/Type.cpp
+++ Practice/Type.cpp
@@ -1,6 +1,7 @@
 
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
@@ -11,6 +12,14 @@ int main () {
 
    cout << "Value is: " << x << endl;
    cout << "Value is: " << fixed << y << endl;
+   cout <<"===================================" << endl;
+
+   char word = 'A';
+   string name(1, word);
+
+   cout << "The Word is: " << word << endl;
+   cout << "The Name is: " << name << endl;
+
 
     return 0;
 }
